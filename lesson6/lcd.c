b5bd93439d5ddb9053c43218f337aa72f4f33ca4
cxiao129 <2857627720@qq.com>
2020-06-23 20:06:09 +0800

    lesson6

---
+++ lesson6/lcd.c
@@ -0,0 +1,22 @@
+#include<LiquidCrystal.h>
+LiquidCrystal lcd(12,11,5,4,3,2);
+
+void setup()
+{
+  lcd.begin(16,2);
+  Serial.begin(9600);
+}
+void loop()
+{
+  lcd.setCursor(0,0);
+  if(Serial.available())
+  {
+    delay(100);
+    lcd.clear();
+    while(Serial.available()>0)
+    {
+      lcd.write(Serial.read());
+      lcd.print(Serial.readString());
+    }
+  }
+}
